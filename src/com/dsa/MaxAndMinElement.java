eef5948577a7b4bbeb763620c2bad282ad46194a
Masoom zafar <masoomzafar294@gmail.com>
2025-09-28 15:41:48 +0530

    changing the value of size

--- src/com/dsa/MaxAndMinElement.java
+++ src/com/dsa/MaxAndMinElement.java
@@ -1,11 +1,11 @@
 package com.dsa;
 public class MaxAndMinElement {
     public static void main(String[] args) {
-        int[] element = {10, 5, 30, 40, 15, 20, 31};
+        int[] element = {10, 5, 30, 40, 15, 20, 50};
         int max = element[0];
         int min = element[0];
 
-        for(int i = 1; i < element.length - 1; i++) {
+        for(int i = 1; i < element.length; i++) {
             if (element[i] > max) {
                 max = element[i];
             } if (element[i] < min) {
